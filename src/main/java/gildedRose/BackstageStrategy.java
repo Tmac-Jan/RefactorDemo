6e9e7b8d2b01062a11282bbe9afac90455c72cda
macmanboy <macmanboy@foxmail.com>
2019-08-19 22:10:37 +0800

    refactor:overwrite method named updateQuality

--- src/main/java/gildedRose/BackstageStrategy.java
+++ src/main/java/gildedRose/BackstageStrategy.java
@@ -10,6 +10,22 @@ package gildedRose;
 public class BackstageStrategy extends ItemStrategy {
     @Override
     public void updateQuality(Item item) {
-        
+        if (item.getQuality() < GildedRose.MAX_QUALITY) {
+            item.setQuality(item.getQuality() + 1);
+            if (item.getSellIn() < 11) {
+                if (item.getQuality() < GildedRose.MAX_QUALITY) {
+                    item.setQuality(item.getQuality() + 1);
+                }
+            }
+            if (item.getSellIn() < 6) {
+                if (item.getQuality() < GildedRose.MAX_QUALITY) {
+                    item.setQuality(item.getQuality() + 1);
+                }
+            }
+        }
+        item.setSellIn(item.getSellIn() - 1);
+        if (item.getSellIn() < 0) {
+            item.setQuality(0);
+        }
     }
 }

f22c628067a31334472ecfbf7fd956f88536e28d
zhanggi3 <gio.zhang@oocl.com>
2019-08-19 19:07:42 +0800

    test:add unit test should_return_item_quality_0_And_sellIn_Minus2_when_call_updateQuality_with_quality_2_And_sellIn_Minus1

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -18,4 +18,17 @@ public class GildedRoseTest {
     assertEquals(0, item.quality);
     assertEquals(0, item.sellIn);
   }
+  @Test
+  public void should_return_item_quality_0_And_sellIn_Minus2_when_call_updateQuality_with_quality_2_And_sellIn_Minus1(){
+    Item[] items = new Item[] { new Item("GioRose", -1, 2) };
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    Item item = Arrays.asList(items).stream().
+      filter(e->e.name.equals("GioRose")).
+      findFirst().orElseThrow(()->new RuntimeException("没有找到"));
+    assertEquals("GioRose", item.name);
+    assertEquals(0, item.quality);
+    assertEquals(-2, item.sellIn);
+  }
+
 }

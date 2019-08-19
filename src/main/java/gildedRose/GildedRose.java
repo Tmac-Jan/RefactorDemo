4338461ddcc45494ef36315b0a5e2a0fc9ad1f23
zhanggi3 <gio.zhang@oocl.com>
2019-08-19 20:22:06 +0800

    refactor,test:  a method named decrementQuality isolated from the method updateQuality

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -5,6 +5,8 @@ public class GildedRose {
   public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT =
       "Backstage passes to a TAFKAL80ETC concert";
   public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+  public static final int MAX_QUALITY = 50;
+  public static final int MIN_QUALITY = 0;
   Item[] items;
 
   public GildedRose(Item[] items) {
@@ -14,24 +16,22 @@ public class GildedRose {
   public void updateQuality() {
     for (int i = 0; i < items.length; i++) {
       if (!isAgedBrie(items[i]) && !isBackstagePass(items[i])) {
-        if (items[i].quality > 0) {
-          if (!isSulfuras(items[i])) {
-            items[i].quality = items[i].quality - 1;
-          }
+        if (items[i].quality > MIN_QUALITY) {
+          decrementQuality(items[i]);
         }
       } else {
-        if (items[i].quality < 50) {
+        if (items[i].quality < MAX_QUALITY) {
           items[i].quality = items[i].quality + 1;
 
           if (isBackstagePass(items[i])) {
             if (items[i].sellIn < 11) {
-              if (items[i].quality < 50) {
+              if (items[i].quality < MAX_QUALITY) {
                 items[i].quality = items[i].quality + 1;
               }
             }
 
             if (items[i].sellIn < 6) {
-              if (items[i].quality < 50) {
+              if (items[i].quality < MAX_QUALITY) {
                 items[i].quality = items[i].quality + 1;
               }
             }
@@ -46,16 +46,14 @@ public class GildedRose {
       if (items[i].sellIn < 0) {
         if (!isAgedBrie(items[i])) {
           if (!isBackstagePass(items[i])) {
-            if (items[i].quality > 0) {
-              if (!isSulfuras(items[i])) {
-                items[i].quality = items[i].quality - 1;
-              }
+            if (items[i].quality > MIN_QUALITY) {
+              decrementQuality(items[i]);
             }
           } else {
             items[i].quality = items[i].quality - items[i].quality;
           }
         } else {
-          if (items[i].quality < 50) {
+          if (items[i].quality < MAX_QUALITY) {
             items[i].quality = items[i].quality + 1;
           }
         }
@@ -74,4 +72,12 @@ public class GildedRose {
   private boolean isAgedBrie(Item item) {
     return item.name.equals(AGED_BRIE);
   }
+  private void decrementQuality(Item item) {
+    if (item.quality > MIN_QUALITY) {
+      if (!isSulfuras(item)) {
+        item.quality = item.quality - 1;
+      }
+    }
+  }
+
 }
